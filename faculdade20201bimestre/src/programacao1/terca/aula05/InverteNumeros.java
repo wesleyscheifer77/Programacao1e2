49918008358881dd5b345028b38276a33ab8caa5
C049463 <C049463@L00000221.copel.nt>
2020-03-03 21:49:52 -0300

    Exercicio invertenumero

--- faculdade20201bimestre/src/programacao1/terca/aula05/InverteNumeros.java
+++ faculdade20201bimestre/src/programacao1/terca/aula05/InverteNumeros.java
@@ -1,5 +1,18 @@
 package programacao1.terca.aula05;
 
-public class InverteNumeros {
+import java.util.Scanner;
 
+public class InverteNumeros {
+	Scanner leia = new Scanner(System.in);
+	public static void main(String[] args) {
+		new InverteNumeros().processar();
+	}
+	private void processar () {
+	
+		for (int i = 100; i > 0; i--) {
+			System.out.println("i:" + i);
+		}
+		
+		this.leia.close();
+	}
 }
